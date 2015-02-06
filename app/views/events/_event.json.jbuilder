4e7a655031e91c00ebdf09c7693670d825d6e2ac
Gabriel Kirkpatrick <g@be-k.biz>
2015-02-05 23:37:34 -0500

    Added initial implementation of users and events

---
+++ app/views/events/_event.json.jbuilder
@@ -0,0 +1,6 @@
+json.id				event.id
+json.name			event.name
+json.description	event.description
+json.start_time		event.start_time
+json.end_time		event.end_time
+json.attending_count	event.users.count
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206000303) do
+ActiveRecord::Schema.define(version: 20150206031432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150206000303) do
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
+
+  create_table "events_users", id: false, force: true do |t|
+    t.integer "event_id"
+    t.integer "user_id"
+  end
+
+  add_index "events_users", ["event_id", "user_id"], name: "index_events_users_on_event_id_and_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
