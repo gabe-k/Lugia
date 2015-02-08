e6627aa145f39345b34a5d48af6b820bf4292fc7
Gabriel Kirkpatrick <g@be-k.biz>
2015-02-08 16:04:57 -0500

    Added location to events output and creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,7 +21,7 @@ class EventsController < ApplicationController
 	end
 
 	def new
-		@event = Event.create(name: params[:name], description: params[:description], start_time: params[:start_time], end_time: params[:end_time], image: params[:image], user: current_user)
+		@event = Event.create(name: params[:name], description: params[:description], location: params[:location], start_time: params[:start_time], end_time: params[:end_time], image: params[:image], user: current_user)
 		@event.add_tags(params[:tags].split(','))
 	end
 end
--- app/views/events/_event.json.jbuilder
+++ app/views/events/_event.json.jbuilder
@@ -1,6 +1,7 @@
 json.id				event.id
 json.name			event.name
 json.description	event.description
+json.location		event.location
 json.image			event.image.url
 json.start_time		event.start_time
 json.end_time		event.end_time
